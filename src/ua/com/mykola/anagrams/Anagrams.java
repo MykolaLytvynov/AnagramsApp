d2b380f11e6c16f88bf4e7fc356f1a1b07465f9a
javalitvinov <seolitvinov@gmail.com>
2021-02-18 13:01:45 +0200

    initial project for dev

--- src/ua/com/mykola/anagrams/Anagrams.java
+++ src/ua/com/mykola/anagrams/Anagrams.java
@@ -1,9 +1,36 @@
 package ua.com.mykola.anagrams;
 
-
+import java.util.Scanner;
 
 public class Anagrams {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("Input phrase, use latin alphabet");
+        String inputphrase = scanner.nextLine();
+        String[] wordsinphrase = inputphrase.split(" ");
+        for (int i = 0; i < wordsinphrase.length; i++) {
+            char[] lettersinwords = wordsinphrase[i].toCharArray();
+            int start = 0;
+            int end = lettersinwords.length-1;
+
+            while (start <= end) {
+                if (Character.isLetter(lettersinwords[start])) {
+                    if (Character.isLetter(lettersinwords[end])) {
+                        char temp = lettersinwords[start];
+                        lettersinwords[start] = lettersinwords[end];
+                        lettersinwords[end] = temp;
+                        end--;
+                        start++;
+                    }
+                    else end--;
+                }
+                else {lettersinwords[start] = lettersinwords[start];
+                    start++;
+                }
+            }
+            String reverseletters = String.valueOf(lettersinwords);
+            System.out.println(reverseletters);
+        }
     }
 }
